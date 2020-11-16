9a39b8f0b550132073b495f4eecbe368fe27b037
crongle <gasmazk73@gmail.com>
2020-11-16 15:29:55 +0000

    Remove build from gitignore.

---
+++ client/build/precache-manifest.7363a7e6f355f02cb56ef292a7ca8602.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822ae18488263b417686ef73cb408ab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db968aa552189c474db1",
+    "url": "/static/css/main.c9211efd.chunk.css"
+  },
+  {
+    "revision": "6a27f6ce423520326e84",
+    "url": "/static/js/2.b7210e53.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b7210e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db968aa552189c474db1",
+    "url": "/static/js/main.61d9776f.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  },
+  {
+    "revision": "34280c5916228bcee0b839abb7884f8e",
+    "url": "/static/media/add.34280c59.svg"
+  },
+  {
+    "revision": "78aa51f72aed8516d6335f7b9446935b",
+    "url": "/static/media/hamburger.78aa51f7.svg"
+  }
+]);
