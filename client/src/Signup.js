06499fa3a974f257e0eb854e329de500734ad59d
crongle <gasmazk73@gmail.com>
2020-11-30 15:30:31 +0000

    Update styling for signup page.

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -6,34 +6,24 @@ import './scss/Signup.scss'
 function Signup() {
   const history = useHistory()
 
+// String content of the submit button which varies if there are errors in the inputs that need to be fixed before the form can be submitted
   const [ submitText, modifySubmitText ] = useState('Submit')
 
 // Validate signup details
   async function validateForm(e) {
     const password = e.target.password.value
     const passwordConfirmation = e.target.passwordConfirmation.value
-    if (
-      (password === passwordConfirmation) &&
-      (password.length > 8))
+    if ((password === passwordConfirmation) && // Passwords match
+        (password.length >= 8)) // Password minimum length is 8 characters
     {
-      console.log('Form OK')
-      return true
+      return true // Form is valid
     }
-    else {
-      console.log('Form BAD')
-      return false
+    else if (password !== passwordConfirmation) {
+      return `Passwords don't match, try retyping them!`
     }
-  }
-
-// Display form errors
-  function displayFormErrorMessage( message ) {
-    modifySubmitText(message)
-    document.getElementById('submit-proxy').classList.toggle('error-styling')
-  }
-
-  function removeFormErrorMessage() {
-    modifySubmitText('Submit')
-    document.getElementById('submit-proxy').classList.remove('error-styling')
+    else if (password.length <= 8) {
+      return `Password too short, make sure it's at least 8 characters long!`
+    }
   }
 
 // Handle form submission
@@ -46,7 +36,7 @@ function Signup() {
     await validateForm(e)
     .then(submissionValidity => {isSubmissionValid = submissionValidity})
 
-    if ( isSubmissionValid )  {
+    if ( isSubmissionValid === true )  {
       const form = e.target
       const data = new FormData(form)
       await fetch('/signup', {
@@ -59,15 +49,29 @@ function Signup() {
           // Username was accepted and account was created
           history.push('/login')
         } else {
+          // Form rejected serverside for username already being in use, display error
           displayFormErrorMessage('Username already in use, try something else!')
         }
       })
     }
     else {
-      console.log('Error with inputs!')
+      displayFormErrorMessage(isSubmissionValid)
     }
   }
 
+
+// Display form errors
+  function displayFormErrorMessage( message ) {
+    modifySubmitText(message)
+    document.getElementById('submit-proxy').classList.add('error-styling')
+  }
+
+  function removeFormErrorMessage() {
+    modifySubmitText('Submit')
+    document.getElementById('submit-proxy').classList.remove('error-styling')
+  }
+
+// Moves and shrinks form input label when the input gains focus
   function moveLabel(e) {
     const label = document.getElementById(e.target.name)
     if (e.target.value === '') {
@@ -88,37 +92,43 @@ function Signup() {
     </div>
 
     <div className='form-area'>
-      <form onSubmit={handleSubmit} name='signup' className='signup-form'>
+      <form onSubmit={handleSubmit} name='signup' className='signup-form' autocomplete='off'>
 
-          <label htmlFor='username' id='username'>
-            Username
-          </label>
-          <input
-            type='text'
-            name='username'
-            onFocus={moveLabel}
-            onBlur={moveLabel}
-            onChange={removeFormErrorMessage}/>
+          <div className='input-container'>
+            <label htmlFor='username' id='username'>
+              Username
+            </label>
+            <input
+              type='text'
+              name='username'
+              onFocus={moveLabel}
+              onBlur={moveLabel}
+              onChange={removeFormErrorMessage}/>
+          </div>
 
-          <label htmlFor='password' id='password'>
-            Password
-          </label>
-          <input
-            type='password'
-            name='password'
-            onFocus={moveLabel}
-            onBlur={moveLabel}/>
+          <div className='input-container'>
+            <label htmlFor='password' id='password'>
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              onFocus={moveLabel}
+              onBlur={moveLabel}/>
+          </div>
 
-          <label htmlFor='passwordConfirmation' id='passwordConfirmation'>
-            Confirm password
-          </label>
-          <input
-            type='password'
-            name='passwordConfirmation'
-            onFocus={moveLabel}
-            onBlur={moveLabel}/>
+          <div className='input-container'>
+            <label htmlFor='passwordConfirmation' id='passwordConfirmation'>
+              Confirm password
+            </label>
+            <input
+              type='password'
+              name='passwordConfirmation'
+              onFocus={moveLabel}
+              onBlur={moveLabel}/>
+          </div>
 
-        <button name='submit-button' id='submit-button' hidden>Sign up</button>
+        <button name='submit-button' id='submit-button' hidden></button>
       </form>
 
       <label id='submit-proxy' htmlFor='submit-button' className='submit-proxy-button'>
@@ -126,6 +136,10 @@ function Signup() {
       </label>
 
     </div>
+
+    <div id='error-box' hidden>Error</div>
+
+
   </div>
   )
 }
