4c82714ec6d2211c3b95713167d49bca7c250dc1
crongle <gasmazk73@gmail.com>
2020-12-03 11:14:34 +0000

    Add styling for login page error handling.

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom'
 
 import './scss/Login.scss'
 
-function Signup({ setUser }) {
+function Login({ setUser }) {
   const history = useHistory()
 
   const [ submitText, modifySubmitText ] = useState('Login')
@@ -21,6 +21,7 @@ function Signup({ setUser }) {
     .then( user => {
       if (user === false ) {
         displayFormErrorMessage()
+        displayErrorBox()
       }
       else {
         setUser(user)
@@ -29,25 +30,41 @@ function Signup({ setUser }) {
     })
   }
 
-  // Form error visualisation
-    // Modify login button to display that there is a problem with the login
-    function displayFormErrorMessage() {
-      modifySubmitText('Username or password incorrect, try again!')
-      document.getElementById('submit-proxy').classList.add('error-styling')
-    }
-    // Revert login button back to normal
-    function removeFormErrorMessage() {
-      modifySubmitText('Login')
-      document.getElementById('submit-proxy').classList.remove('error-styling')
-    }
-    // Clear error messages when any of the form input values change
-    function clearErrorDisplay() {
-      removeFormErrorMessage()
-    }
+// Form error visualisation
+  // Modify login button to display that there is a problem with the login
+  function displayFormErrorMessage() {
+    modifySubmitText('Problem logging in!')
+    document.getElementById('submit-proxy').classList.add('error-styling')
+  }
+  // Revert login button back to normal
+  function removeFormErrorMessage() {
+    modifySubmitText('Login')
+    document.getElementById('submit-proxy').classList.remove('error-styling')
+  }
+  // Display error box in form
+  function displayErrorBox() {
+    document.getElementById('form-container').classList.add('shrink-width')
+    const errorBox = document.getElementById('error-box')
+    let errorItem = document.createElement('div')
+    errorItem.textContent = 'Problem with username or password, try retyping and logging in again.'
+    errorBox.appendChild(errorItem)
+    errorBox.classList.add('visible')
+  }
+  // Remove error box in form
+  function removeErrorBox() {
+    document.getElementById('form-container').classList.remove('shrink-width')
+    const errorBox = document.getElementById('error-box')
+    errorBox.classList.remove('visible')
+    errorBox.textContent = ''
+  }
+  // Clear error messages when any of the form input values change
+  function clearErrorDisplay() {
+    removeErrorBox()
+    removeFormErrorMessage()
+  }
 
 
-
-  // Moves fake placeholder text on inputs move and stay at the new position as long as the input isn't empty
+// Moves fake placeholder text on inputs move and stay at the new position as long as the input isn't empty
   function moveLabel(e) {
     const label = document.getElementById(e.target.name)
     if (e.target.value === '') {
@@ -71,7 +88,7 @@ function Signup({ setUser }) {
 
       <div className='form-area'>
         <form onSubmit={handleSubmit} name='login' className='login-form'>
-
+          <div id='form-container'>
             <div className='input-container'>
               <label htmlFor='username' id='username'>
                 Username
@@ -95,17 +112,18 @@ function Signup({ setUser }) {
                 onBlur={moveLabel}
                 onChange={clearErrorDisplay}/>
             </div>
+            <div id='error-box'>Error</div>
 
-          <button name='submit-button' id='submit-button' hidden></button>
+            <button name='submit-button' id='submit-button' hidden></button>
+          </div>
         </form>
 
         <label id='submit-proxy' htmlFor='submit-button' className='submit-proxy-button'>
           {submitText}
         </label>
-
       </div>
     </div>
   )
 }
 
-export default Signup
+export default Login
